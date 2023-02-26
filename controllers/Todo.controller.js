82f25fe924c466f4c234e68cd817867bf8fd0c32
DESKTOP-OF-TRUN\Admin <vstrungnguyen@gmail.com>
2023-02-26 09:56:33 +0700

    UD-26-02-2023: Update function to handle rank todos of boards

--- controllers/Todo.controller.js
+++ controllers/Todo.controller.js
@@ -4,17 +4,12 @@ const TodoService = require('../services/Todo.service');
 const router = express.Router();
 
 router.post("/", createTodo)
-router.get("/:id", getTodoById)
 router.put("/:id", editTodo)
 router.delete("/:id", deleteTodo)
 router.put("/rank/:id", rankTodo)
 
 function createTodo(req, res) {
     TodoService.createTodo(req, res)
-}
-
-function getTodoById(req, res) {
-    TodoService.getTodoById(req, res)
 }
 
 function editTodo(req, res) {
